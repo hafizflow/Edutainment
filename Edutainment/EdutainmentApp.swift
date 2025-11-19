2d8ba007692277c64d7998d6299d492a96422111
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-20 04:12:23 +0600

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Hafizur Rahman on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
